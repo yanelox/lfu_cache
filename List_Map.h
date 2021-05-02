31f026a19a51f50880d3f91c0c84a65dba476c9a
Ekaterina <70318220+Keiko-Chan@users.noreply.github.com>
2021-05-02 13:55:32 +0300

    Create List_Map.h

---
+++ List_Map.h
@@ -0,0 +1,43 @@
+#pragma once
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "Hash_Map.h"
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+struct lfu_node;
+struct freq_node;
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+struct freq_node* create_freq(int freq_dat, struct freq_node* prev_fr);
+struct lfu_node* create_lfu(DATA* lfu_dat, struct freq_node* parent_lfu, struct lfu_node* prev_lfu);
+void remove_freq(struct freq_node* del);
+void remove_lfu(struct freq_node* head);
+void replace_lfu(struct lfu_node* cur_lfu);
+struct freq_node* create_head();
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+struct lfu_node
+{
+    struct request_t* data_t;
+    struct lfu_node * next;
+    struct lfu_node * prev;
+
+    struct freq_node* parent;
+
+};
+
+struct freq_node
+{
+    int freq_t;
+    struct freq_node* next;
+    struct freq_node* prev;
+
+    struct lfu_node * child;
+
+};
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
